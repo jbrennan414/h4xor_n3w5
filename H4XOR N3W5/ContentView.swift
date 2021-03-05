8d0aace030c8b3c47e91fecfc969ce0bdfbbf453
John Brennan <jbrennan@fluentstream.com>
2021-03-04 18:58:32 -0700

    Initial Commit

---
+++ H4XOR N3W5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR N3W5
+//
+//  Created by John Brennan on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
